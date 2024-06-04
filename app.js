03e5883efa76154cbcc6c2708310dca572918820
OMAR <rockstonestudios666@gmail.com>
2024-06-03 20:55:04 -0500

    Sin Credentials

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors({
   origin: '*', 
   methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
 
-credentials: true }));
+credentials: false}));
 app.use(express.json());
 app.use(cookieParser());
 
