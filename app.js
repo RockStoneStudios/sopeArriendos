7721fef47864d42d69c8f42cfc34aa675e4eb1f7
OMAR <rockstonestudios666@gmail.com>
2024-06-04 10:11:09 -0500

    Twelve Commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors({ 
   origin: '*', 
   methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  credentials : true
+  credentials : false
 }));
 app.use(express.json());
 app.use(cookieParser());
