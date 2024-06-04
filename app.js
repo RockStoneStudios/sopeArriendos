67d680eea7110044b73446436197cf6afb1b7501
OMAR <rockstonestudios666@gmail.com>
2024-06-03 20:26:21 -0500

    Cors update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors({ 
   origin: '*', 
   methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization',
+
 credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
